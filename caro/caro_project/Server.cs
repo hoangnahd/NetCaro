3448d5cb16e37d99a9fbd0f7af037401a9151dd6
Hoang Anh <22520041@gm.uit.edu.vn>
2024-06-02 10:21:36 +0700

    last change

--- caro/caro_project/Server.cs
+++ caro/caro_project/Server.cs
@@ -1,118 +1,103 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Net;
+using System.Linq;
 using System.Net.Sockets;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Threading;
 using Newtonsoft.Json;
+using System.Collections.Concurrent;
 
 namespace caro_project
 {
     internal class Server
     {
         private TcpListener tcpListener;
-        private ConcurrentDictionary<TcpClient, NetworkStream> clients = new ConcurrentDictionary<TcpClient, NetworkStream>();
+        private List<TcpClient> clients = new List<TcpClient>();
         private List<Player> players = new List<Player>();
         private bool isFull = false;
+        private int port = Form1.roomId;
 
         public void Connect()
         {
-            tcpListener = new TcpListener(IPAddress.Any, 1234); // Bind to all available network interfaces on port 1234
-            tcpListener.Start(); // Start listening for incoming connections
+            tcpListener = new TcpListener(IPAddress.Any, port);
+            tcpListener.Start();
 
-            Console.WriteLine("Server started. Waiting for clients...");
-            AcceptClients(); // Start accepting client connections
-        }
-
-        private void AcceptClients()
-        {
+            // Start accepting client connections asynchronously
             tcpListener.BeginAcceptTcpClient(new AsyncCallback(HandleClientConnection), null);
         }
-
         private void HandleClientConnection(IAsyncResult ar)
         {
-            TcpClient client = tcpListener.EndAcceptTcpClient(ar);
-            NetworkStream stream = client.GetStream();
-
             if (clients.Count >= 2)
             {
+                TcpClient Client = tcpListener.EndAcceptTcpClient(ar);
+                NetworkStream stream = Client.GetStream();
                 byte[] buffer = Encoding.UTF8.GetBytes("Room is full");
                 stream.Write(buffer, 0, buffer.Length);
                 stream.Close();
-                client.Close();
-            }
-            else
-            {
-                if (clients.TryAdd(client, stream))
-                {
-                    Console.WriteLine("Client connected.");
-                    // Start listening for messages from the client
-                    Thread receiveThread = new Thread(new ParameterizedThreadStart(ReceiveData));
-                    receiveThread.Start(client);
-                }
+                Client.Close();
+                return; // If two clients are already connected, do not accept more connections
             }
 
-            AcceptClients(); // Continue accepting more client connections
+            TcpClient client = tcpListener.EndAcceptTcpClient(ar);
+            clients.Add(client);
+
+            // Start listening for messages from the client
+            Thread receiveThread = new Thread(new ParameterizedThreadStart(ReceiveData));
+            receiveThread.Start(client);
+
+            // Continue accepting more client connections
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(HandleClientConnection), null);
         }
+
 
         private void ReceiveData(object clientObj)
         {
             TcpClient client = (TcpClient)clientObj;
-            NetworkStream stream = clients[client];
+            NetworkStream stream = client.GetStream();
 
             byte[] buffer = new byte[2048];
             int bytesRead;
 
-            try
+            while (true)
             {
-                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
-                {
-                    string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    List<Player> jsonData = JsonConvert.DeserializeObject<List<Player>>(dataReceived);
-                    lock (players)
-                    {
-                        if (players.Count < 2)
-                        {
-                            players.Add(jsonData[0]);
-                        }
-                        else
-                        {
-                            players = jsonData;
-                            isFull = true;
-                        }
-                        Broadcast(JsonConvert.SerializeObject(players));
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
-            finally
-            {
-                // Clean up when client disconnects
-                Console.WriteLine("Client disconnected.");
-                stream.Close();
-                client.Close();
-                clients.TryRemove(client, out _);
-            }
-        }
-
-        private void Broadcast(string message)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes(message);
-            foreach (var client in clients.Keys)
-            {
-                NetworkStream stream = clients[client];
                 try
                 {
-                    stream.Write(buffer, 0, buffer.Length);
+                    // Read data from the client
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    List<Player> jsonData = JsonConvert.DeserializeObject<List<Player>>(dataReceived);
+                    if (players.Count != 2)
+                    {
+                        players.Add(jsonData[0]);
+                    }
+                    else
+                    {
+                        players = jsonData;
+                        isFull = true;
+                    }
+                        
+
+                    Broadcast(JsonConvert.SerializeObject(players));
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Broadcast error: {ex.Message}");
+                    Console.WriteLine($"Error: {ex.Message}");
+                    break;
                 }
+            }
+            stream.Close();
+            client.Close();
+        }
+        private void Broadcast(string message)
+        {
+            foreach (TcpClient client in clients)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(buffer, 0, buffer.Length);
             }
         }
     }
