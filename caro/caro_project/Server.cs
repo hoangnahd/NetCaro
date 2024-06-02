3e97abd413ca649904d0e9f52325e7fa0c88a1ae
Hoang Anh <22520041@gm.uit.edu.vn>
2024-06-02 09:37:32 +0700

    fix port

--- caro/caro_project/Server.cs
+++ caro/caro_project/Server.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 using Newtonsoft.Json;
+using System.Collections.Concurrent;
 
 namespace caro_project
 {
@@ -16,41 +18,39 @@ namespace caro_project
         private List<TcpClient> clients = new List<TcpClient>();
         private List<Player> players = new List<Player>();
         private bool isFull = false;
-        private bool isRunning = false;
-
+        private int port = Form1.roomId;
         public void Connect()
         {
-            tcpListener = new TcpListener(IPAddress.Any, 1234);
+            tcpListener = new TcpListener(IPAddress.Any, port);
             tcpListener.Start();
-            isRunning = true;
 
             // Start accepting client connections asynchronously
             tcpListener.BeginAcceptTcpClient(new AsyncCallback(HandleClientConnection), null);
         }
-
         private void HandleClientConnection(IAsyncResult ar)
         {
-            if (clients.Count >= 2 || !isRunning)
+            if (clients.Count >= 2)
             {
-                TcpClient client = tcpListener.EndAcceptTcpClient(ar);
-                NetworkStream stream = client.GetStream();
+                TcpClient Client = tcpListener.EndAcceptTcpClient(ar);
+                NetworkStream stream = Client.GetStream();
                 byte[] buffer = Encoding.UTF8.GetBytes("Room is full");
                 stream.Write(buffer, 0, buffer.Length);
                 stream.Close();
-                client.Close();
+                Client.Close();
                 return; // If two clients are already connected, do not accept more connections
             }
 
-            TcpClient tcpClient = tcpListener.EndAcceptTcpClient(ar);
-            clients.Add(tcpClient);
+            TcpClient client = tcpListener.EndAcceptTcpClient(ar);
+            clients.Add(client);
 
             // Start listening for messages from the client
             Thread receiveThread = new Thread(new ParameterizedThreadStart(ReceiveData));
-            receiveThread.Start(tcpClient);
+            receiveThread.Start(client);
 
             // Continue accepting more client connections
             tcpListener.BeginAcceptTcpClient(new AsyncCallback(HandleClientConnection), null);
         }
+
 
         private void ReceiveData(object clientObj)
         {
@@ -60,7 +60,7 @@ namespace caro_project
             byte[] buffer = new byte[2048];
             int bytesRead;
 
-            while (isRunning)
+            while (true)
             {
                 try
                 {
@@ -77,6 +77,7 @@ namespace caro_project
                         players = jsonData;
                         isFull = true;
                     }
+                        
 
                     Broadcast(JsonConvert.SerializeObject(players));
                 }
@@ -89,7 +90,6 @@ namespace caro_project
             stream.Close();
             client.Close();
         }
-
         private void Broadcast(string message)
         {
             foreach (TcpClient client in clients)
@@ -98,40 +98,6 @@ namespace caro_project
                 byte[] buffer = Encoding.UTF8.GetBytes(message);
                 stream.Write(buffer, 0, buffer.Length);
             }
-        }
-
-        public void Stop()
-        {
-            // Stop the server
-            isRunning = false;
-
-            // Stop listening for new clients
-            if (tcpListener != null)
-            {
-                tcpListener.Stop();
-            }
-
-            // Close all active client connections
-            foreach (var client in clients)
-            {
-                try
-                {
-                    if (client.Connected)
-                    {
-                        client.GetStream().Close();
-                        client.Close();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error closing client: {ex.Message}");
-                }
-            }
-
-            // Clear the client and player lists
-            clients.Clear();
-            players.Clear();
-            isFull = false;
         }
     }
 }
